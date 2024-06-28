edd414087dab277675065d9fd36d8663a30ca270
Annya <annya.host@gmail.com>
2024-06-28 19:46:58 +0800

    parse digits(hex,dec,binary)

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,14 @@
 extern crate proc_macro;
 
 use macros::register_macro;
-use nom::{branch::alt, bytes::complete::tag, error::Error, IResult};
+use nom::{
+    branch::alt,
+    bytes::complete::{is_a, tag, take_while},
+    combinator::map_res,
+    error::Error,
+    sequence::preceded,
+    IResult,
+};
 
 use crate::{instruction::Operand, register::Register};
 
@@ -54,6 +61,39 @@ pub fn immediate(input: &str) -> IResult<&str, OperandBox> {
     unimplemented!()
 }
 
+fn digits(input: &str) -> IResult<&str, u64> {
+    alt((binary_digits, hex_digits, decimal_digits))(input)
+}
+
+fn binary_digits(input: &str) -> IResult<&str, u64> {
+    map_res(preceded(tag("0b"), take_while(is_binary)), from_binary)(input)
+}
+fn hex_digits(input: &str) -> IResult<&str, u64> {
+    map_res(preceded(tag("0x"), take_while(is_hex_digit)), from_hex)(input)
+}
+fn decimal_digits(input: &str) -> IResult<&str, u64> {
+    map_res(take_while(is_digit), from_decimal)(input)
+}
+fn is_binary(c: char) -> bool {
+    c.is_digit(2)
+}
+fn is_digit(c: char) -> bool {
+    c.is_digit(10)
+}
+fn is_hex_digit(c: char) -> bool {
+    c.is_digit(16)
+}
+
+fn from_decimal(input: &str) -> Result<u64, std::num::ParseIntError> {
+    u64::from_str_radix(input, 10)
+}
+fn from_binary(input: &str) -> Result<u64, std::num::ParseIntError> {
+    u64::from_str_radix(input, 2)
+}
+fn from_hex(input: &str) -> Result<u64, std::num::ParseIntError> {
+    u64::from_str_radix(input, 16)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::register::Len;
@@ -68,6 +108,16 @@ mod tests {
         for (input, expect) in inputs.iter().zip(expectes.iter()) {
             let (remain, reg) = register(input).expect("cannot parse rax");
             assert_eq!(reg.get_register().unwrap(), *expect);
+            assert!(remain.len() == 0);
+        }
+    }
+    #[test]
+    fn test_digits_parse() {
+        let inputs = ["0xdeadbeef", "31415926", "0b10010"];
+        let expectes = [0xdeadbeef, 31415926, 0b10010];
+        for (input, expect) in inputs.iter().zip(expectes.iter()) {
+            let (remain, reg) = digits(input).expect("cannot parse rax");
+            assert_eq!(reg, *expect);
             assert!(remain.len() == 0);
         }
     }
